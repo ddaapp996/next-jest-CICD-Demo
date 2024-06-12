3cfbd0004658defcc32ca556b7661d9699e2495b
ddaapp996 <anhpd.soict.hust@gmail.com>
2024-06-12 19:11:15 +0700

    update

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -5,7 +5,7 @@ import styles from "@/pages/index.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <img src="https://i.pinimg.com/1200x/40/c5/95/40c59547dee3bd2ac4dd2ba88bfff741.jpg" alt="" srcset="" />
+      <img src="https://i.pinimg.com/1200x/40/c5/95/40c59547dee3bd2ac4dd2ba88bfff741.jpg" alt="" />
       <p>
         <h1>Hoàng Tường Vy chưa làm bài tập Elsa</h1>
         <h1>Hoàng Anh Khoa ít đi chơi lại</h1>
